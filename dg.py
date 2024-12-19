290b3c527404bd31a497f892b3b20d93d8edb335
Pavan Behara <pavan.behara@openforcefield.org>
2024-12-19 14:51:57 -0800

    changes that didn't get pushed in last commit

--- dg.py
+++ dg.py
@@ -108,12 +108,13 @@ def plot_mbar_overlap_matrix(matrix, skip_lambda_index=[], ax=None):
     ax.set_ylim(0, size + 1)
     return ax
 
-def test(path, repeat, lambdanr):
+def test(path, lambdanr):
     counts = []
     for i in range(0, lambdanr):
         count = 0
         start = False
-        xvg = '%i/lambda_%i/Production_MD/prod.xvg' %(repeat,i)
+        xvg = 'lambda_%i/Production_MD/prod.xvg' %(i)
+        # xvg = '%i/lambda_%i/Production_MD/prod.xvg' %(repeat,i)
         u_nk = open(xvg,'r')
         for x in u_nk:
             if x.startswith('0.0000'):
@@ -133,86 +134,97 @@ discard = 1000
 start = discard+1000
 #evaluate the dG every 1000ps for cumulative dG analysis (assess convergence)
 protocols = ['step1']
-repeats = [1]
-for r in repeats:
-    for p in protocols:
-        if p == 'step1':
-            lambdanr = 14
-        try:
-            path = ""
-            upper = test(path,r,lambdanr)
-            upper = int(upper)
-            end = upper+1
-            print("start", start, "end", end)
-            timeslices = list(range(start,end,1000))
-            print("timeslices:", timeslices)
-            u_nks_all = []
-            print(r)
+#repeats = [1]
+#for r in repeats:
+for p in protocols:
+    if p == 'step1':
+        lambdanr = 20
+    try:
+        path = ""
+        upper = test(path,lambdanr)
+        #upper = test(path,r,lambdanr)
+        upper = int(upper)
+        end = upper+1
+        print("start", start, "end", end)
+        timeslices = list(range(start,end,1000))
+        print("timeslices:", timeslices)
+        u_nks_all = []
+        #print(r)
+        for i in range(0, lambdanr):
+            folder = 'lambda_%i/Production_MD'%(i)
+            xvg = '%s/prod.xvg' %(folder)
+            u_nk = extract_u_nk(xvg, T=303.15)
+            print(len(u_nk))
+            u_nks_all.append(u_nk)
+        dGs = []
+        dG_error = []
+        for time in timeslices:
+            print("TIME:", time)
+            u_nks = []
             for i in range(0, lambdanr):
-                folder = '%i/lambda_%i/Production_MD'%(r, i)
-                xvg = '%s/prod.xvg' %(folder)
-                u_nk = extract_u_nk(xvg, T=298.15)
-                print(len(u_nk))
-                u_nks_all.append(u_nk)
-            dGs = []
-            dG_error = []
-            for time in timeslices:
-                print("TIME:", time)
-                u_nks = []
-                for i in range(0, lambdanr):
-                    #Slice data up to that time frame
-                    u_nk = slicing(u_nks_all[i], upper=time, lower=discard)
-                    #choose the potential energy of the lambda state of the trajectory for autocorreltation analysis
-                    #could be changed to always using the potential energy of the same lambda state
-                    series = u_nk[u_nk.columns[i]]
-                    #run autocorrelation analysis
-                    statinef  = PTstatistical_inefficiency(u_nk[u_nk.columns[i]], fast=False)
-                    indices = subsample_correlated_data(series, g=statinef,
-                                                      conservative=True)
-                    #If there are less than 100 uncorrelated samples: use 100 samples as default
-                    if len(indices) >= 100:
-                        u_nk = u_nk.iloc[indices]
-                    else:
-                        #A low number of uncorrelated samples can indicate a slow motion
-                        print('time %i: Low number of uncorrelated samples (%i) in lambda %i. Check for slow motion.'%(time, len(indices), i)) 
-                        #number of data points in this lambda window
-                        number = int(len(u_nk.index))
-                        #extract 100 data points
-                        skip = int(number/100)
-                        #Slice data to obtain 100 samples
-                        u_nk = slicing(u_nk, step=skip)
-                    u_nks.append(u_nk)
-                #Estimate dG using alchemlyb/pymbar
-                u_nk = pd.concat(u_nks)
-                index_2 = u_nk.index.values
-                mbar = MBAR()
-                fit = mbar.fit(u_nk)
-                groups = u_nk.groupby(level=u_nk.index.names[1:])
-                N_k = [(len(groups.get_group(i)) if i in groups.groups else 0) for i in u_nk.columns]       
-                lam =  mbar.delta_f_.columns.values.tolist()
-                endpoint_diff = 0.596 * mbar.delta_f_.loc[[lam[0]], [lam[-1]]]
-                dg_diff = round(float(endpoint_diff.iloc[0][lam[-1]]),3)
-                dGs.append(dg_diff)
-                uncertainty = 0.596 * mbar.d_delta_f_.loc[[lam[0]], [lam[-1]]]
-                uncertainty = round(float(uncertainty.iloc[0][lam[-1]]),3)
-                dG_error.append(uncertainty)
-                if time == timeslices[-1]:
-                    ####Overlap Matrix
-                    u_kn = u_nk.transpose()
-                    mbar_pymbar = pymbar.mbar.MBAR(u_kn, N_k)
-                    overlap_matrix = mbar_pymbar.compute_overlap()
-                    overlap_matrix = overlap_matrix['matrix']
-                    ax = plot_mbar_overlap_matrix(overlap_matrix)
-                    ax.figure.savefig('O_MBAR.pdf', bbox_inches='tight', pad_inches=0.0)    
-            print(dGs)
-            print(dG_error)
-            file = open('%i/dg.pickle'%(r), 'wb')
-            pickle.dump(dGs, file)
-            file.close()
-            file = open('%i/dg_error.pickle'%(r), 'wb')
-            pickle.dump(dG_error, file)
-            file.close()
-        except (ValueError, ) as e:
-            print(f"!!!Error: {e}")
-            continue
+                #Slice data up to that time frame
+                u_nk = slicing(u_nks_all[i], upper=time, lower=discard)
+                #choose the potential energy of the lambda state of the trajectory for autocorreltation analysis
+                #could be changed to always using the potential energy of the same lambda state
+                series = u_nk[u_nk.columns[i]]
+                #run autocorrelation analysis
+                statinef  = PTstatistical_inefficiency(u_nk[u_nk.columns[i]], fast=False)
+                indices = subsample_correlated_data(series, g=statinef,
+                                                  conservative=True)
+                #If there are less than 100 uncorrelated samples: use 100 samples as default
+                if len(indices) >= 100:
+                    u_nk = u_nk.iloc[indices]
+                else:
+                    #A low number of uncorrelated samples can indicate a slow motion
+                    print('time %i: Low number of uncorrelated samples (%i) in lambda %i. Check for slow motion.'%(time, len(indices), i)) 
+                    #number of data points in this lambda window
+                    number = int(len(u_nk.index))
+                    #extract 100 data points
+                    skip = int(number/100)
+                    #Slice data to obtain 100 samples
+                    u_nk = slicing(u_nk, step=skip)
+                u_nks.append(u_nk)
+            #Estimate dG using alchemlyb/pymbar
+            u_nk = pd.concat(u_nks)
+            index_2 = u_nk.index.values
+            mbar = MBAR()
+            print(u_nk.head())
+            fit = mbar.fit(u_nk)
+            groups = u_nk.groupby(level=u_nk.index.names[1:])
+            N_k = [(len(groups.get_group(i)) if i in groups.groups else 0) for i in u_nk.columns]       
+            lam =  mbar.delta_f_.columns.values.tolist()
+            endpoint_diff = 0.596 * mbar.delta_f_.loc[[lam[0]], [lam[-1]]]
+            dg_diff = round(float(endpoint_diff.iloc[0][lam[-1]]),3)
+            dGs.append(dg_diff)
+            uncertainty = 0.596 * mbar.d_delta_f_.loc[[lam[0]], [lam[-1]]]
+            uncertainty = round(float(uncertainty.iloc[0][lam[-1]]),3)
+            dG_error.append(uncertainty)
+            if time == timeslices[-1]:
+                ####Overlap Matrix
+                u_kn = u_nk.transpose()
+                mbar_pymbar = pymbar.mbar.MBAR(u_kn, N_k)
+                overlap_matrix = mbar_pymbar.compute_overlap()
+                overlap_matrix = overlap_matrix['matrix']
+                ax = plot_mbar_overlap_matrix(overlap_matrix)
+                ax.figure.savefig('O_MBAR.pdf', bbox_inches='tight', pad_inches=0.0)    
+        print(dGs)
+        print(dG_error)
+        file = open('dg.pickle', 'wb')
+        pickle.dump(dGs, file)
+        file.close()
+        file = open('dg_error.pickle', 'wb')
+        pickle.dump(dG_error, file)
+        file.close()
+    except (ValueError, ) as e:
+        print(f"!!!Error: {e}")
+        continue
+
+#Plot the cumulative dG estimate as a function of simulation time in ns
+x_e = np.arange(start/1000,len(dGs)+start/1000,1)
+plt.errorbar(x_e, dGs, yerr=dG_error, alpha=0.5, fmt = '-o', mfc='w', mew=2.5, lw=5, ms=10)
+plt.xlabel('simulation time in ns',fontsize=20)
+plt.ylabel('ΔG in kcal/mol',fontsize=20)
+plt.grid(True)    
+plt.savefig('dG.png', bbox_inches = "tight")
+plt.show()
 
